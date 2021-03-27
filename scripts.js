fd0e7b31c516d4486a97190ac38d24b75cc2f4ea
Guilherme Lordêlo Seixas Vitória <61258019+gui-vitoria@users.noreply.github.com>
2021-03-27 02:53:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,13 @@
+//Mensagem para o usuário
 alert("Olá astronauta. Ta com preguiça de fazer o cálculo? Pois bem,vou te ajudar com isso")
 
+//Pergutando valor a ser convertido
 var anosLuz = parseFloat(prompt("Qual a distância em anos-luz que você quer converter?"))
 
+//Converter anos luz em kmeee
 var KM = anosLuz * 9460528404879.4
 
+//Mostar o resultado
 alert("Esse valor convertido para km fica " + KM.toFixed(2) + " km")
 
 alert("Rapaz, é quilômetro viu, cansei só de pensar😅")
