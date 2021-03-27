4fe40b9b6c8dc78f74c09849fe45a4206a79df82
Guilherme Lordêlo Seixas Vitória <61258019+gui-vitoria@users.noreply.github.com>
2021-03-27 02:48:00 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,31 +1,9 @@
-//Criando variável para o usuário decidir os 2 valores
-var primeiroValor = parseInt(prompt("Digite o primeiro valor: "))
-var segundoValor = parseInt(prompt("Digite o segundo valor: "))
+alert("Olá astronauta. Ta com preguiça de fazer o cálculo? Pois bem,vou te ajudar com isso")
 
-//Perguntando ao usuário qual a operação fazer
-var operacao = prompt("Escolha a operação desejada: \n1 - Multiplicação\n2 - Divisão\n3 - Soma\n4 - Substração")
+var anosLuz = parseFloat(prompt("Qual a distância em anos-luz que você quer converter?"))
 
-if(operacao == 1){
-  //Operação a ser feita exibida em todos os ifs
-  var resultado = primeiroValor * segundoValor
-  
-  //Comando para ler as variáveis para serem escritas   na tela entre o <h2> e </h2> do html que se repetirá   em todos os ifs
-  document.write("<h2>", primeiroValor, " x ",           segundoValor, " = ", resultado, "</h2>")
-  
-}else if(operacao == 2){
-  var resultado = primeiroValor / segundoValor
-  
-  document.write("<h2>", primeiroValor, " / ",           segundoValor, " = ", resultado, "</h2>")
-    
-}else if(operacao == 3){
-  var resultado = primeiroValor + segundoValor
-  
-  document.write("<h2>", primeiroValor, " + ",           segundoValor, " = ", resultado, "</h2>")
-    
-}else if(operacao == 4){
-  var resultado = primeiroValor - segundoValor  
-  
-  document.write("<h2>", primeiroValor, " - ",           segundoValor, " = ", resultado, "</h2>")  
-}else{
-  document.write("<h2>", "operação ERROR", "</h2>")   
-}
+var KM = anosLuz * 9460528404879.4
+
+alert("Esse valor convertido para km fica " + KM.toFixed(2) + " km")
+
+alert("Rapaz, é quilômetro viu, cansei só de pensar😅")
